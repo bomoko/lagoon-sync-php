f45d83490b69ee8d013a205cd6ef0e48461d103e
Blaize Kaye <blaize.kaye@amazee.com>
2020-11-08 05:59:46 +1300

    Adds new sync logic

--- src/Command/SyncCommand.php
+++ src/Command/SyncCommand.php
@@ -20,16 +20,23 @@ class SyncCommand extends Command
     {
         $this->addArgument('remote', InputArgument::REQUIRED,
           'Which REMOTE environment are we syncing from? (openshiftprojectname)')
-          ->addArgument('resource', InputArgument::OPTIONAL,
+          ->addArgument('resource', InputArgument::REQUIRED,
             'Which resource are we syncing?');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $syncConfigDetails = SyncConfig::loadSyncConfigFromFile();
-        $syncer = new MariadbSyncer($syncConfigDetails);
+        $resource = $input->getArgument('resource');
+
+        $classname = ucfirst($resource) . 'Syncer';
+        if(!class_exists($classname)) {
+            throw new \Exception("Syncing of resource type $classname not supported");
+        }
+
+        $syncer = new $classname($syncConfigDetails);
         $syncRunner = new SyncRunner($syncer, $input->getArgument('remote'));
-        $syncRunner->run();
+//        $syncRunner->run();
         return Command::SUCCESS;
     }
 
